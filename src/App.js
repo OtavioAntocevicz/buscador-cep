6eae70ead39c02a4eae7eeae4ab7119ea12c7907
OtavioAntocevicz <otavioantocevicz@gmail.com>
2023-06-16 18:10:55 -0300

    Commit inicial

---
+++ src/App.js
@@ -0,0 +1,67 @@
+import { useState } from 'react';
+import { FiSearch } from 'react-icons/fi';
+import './style.css';
+
+import api from './services/api';
+
+
+function App() {
+
+  const [input, setInput] = useState('');
+  const [cep, setCep] = useState({});
+  
+  async function handleSearch(){
+    
+    
+    if(input === ''){
+      alert("Preencha com um CEP!")
+      return;
+    }
+
+    try{
+      const response = await api.get(`${input}/json`);
+      setCep(response.data);
+      setInput("");
+
+    }catch{
+      alert("Ops, erro ao buscar!");
+      setInput("");
+    }
+
+  }
+
+
+  return (
+    <div className="container">
+      <h1 className="title">Buscador de CEP</h1>
+
+      <div className="containerInput">
+        <input
+          type="text"
+          placeholder="Digite seu CEP..."
+          value={input}
+          onChange={(event) => setInput(event.target.value)}
+        />
+
+        <button className="buttonSearch" onClick={handleSearch} id='enviar'>
+          <FiSearch size={25} color="#fff"/>
+        </button>
+      </div>
+
+
+      {Object.keys(cep).length > 0 && (
+        <main className="main">
+          <h2>CEP: {cep.cep}</h2>
+
+          <span>{cep.logradouro}</span>
+          <span>Complento: {cep.complemento}</span>
+          <span>{cep.bairro}</span>
+          <span>{cep.localidade} - {cep.uf}</span>
+        </main>
+      )}
+
+    </div>
+  );
+};
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+document.addEventListener("keypress", function(e){
+
+  if(e.key === "Enter") {
+    const btn = document.querySelector("#enviar");
+
+    btn.click();
+  }
+
+});
